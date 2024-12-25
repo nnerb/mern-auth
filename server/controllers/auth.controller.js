aa449989d822b4ba810f88c7106f691f5902a3fb
Brenn Santiago <brennaldwin.santiago@gmail.com>
2024-12-25 19:03:39 +0800

    define routes and controllers

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.send('This is the signup route');
+}
+
+export const login = async (req, res) => {
+  res.send('This is the login route');
+}
+
+export const logout = async (req, res) => {
+  res.send('This is the logout route');
+}
--- server/index.js
+++ server/index.js
@@ -1,20 +1,21 @@
 import express from 'express';
 import { connectDB } from './db/connect-db.js';
 import dotenv from "dotenv";
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
 const app = express();
+
+app.use("/api/auth", authRoutes)
 const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World 123');
+});
 
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on port ${PORT}`);
 })
 
-app.get('/', (req, res) => {
-  res.send('Hello World 123');
-});
-
-// 8zyqOdvvVXnaBS84
-// mongodb+srv://brennaldwinsantiago:8zyqOdvvVXnaBS84@cluster0.a7cw4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
