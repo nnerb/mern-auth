a4ee99b72080db3cb51284562e37af632b0cee60
Brenn Santiago <brennaldwin.santiago@gmail.com>
2024-12-28 22:19:28 +0800

    create signup page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,7 @@
+import { Route, Routes } from "react-router-dom"
 import FloatingShape from "./components/floating-shape"
+import SignUpPage from "./pages/signup"
+import LoginPage from "./pages/login"
 
 function App() {
   return (
@@ -10,6 +13,11 @@ function App() {
     <FloatingShape color="bg-green-500" size="w-64 h-64" top="-5%" left="10%" delay={0} />
     <FloatingShape color="bg-emerald-500" size="w-48 h-48" top="70%" left="80%" delay={5} />
     <FloatingShape color="bg-lime-500" size="w-32 h-32" top="40%" left="-10%" delay={2} />
+    <Routes>
+      <Route path="/" element={"Home"}></Route>
+      <Route path="/signup" element={<SignUpPage/>}></Route>
+      <Route path="/login" element={<LoginPage/>}></Route>
+    </Routes>
    </div>
   )
 }
---
+++ client/src/pages/login.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+  return ( 
+    <section>
+      This is the login page!
+    </section>
+   );
+}
+ 
+export default LoginPage;
